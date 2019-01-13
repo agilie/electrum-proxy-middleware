e471e9bb68978b0e353811a69505437fcd1f800b
SergeyMell <sergey.mell@agilie.com>
2019-01-13 20:26:45 +0200

    Added swagger documentations

---
+++ swaggerfile.js
@@ -0,0 +1,9 @@
+module.exports = {
+    info: {
+        title: 'Electrum proxy middleware',
+        version: '1.0.0', // Version (required)
+        description: 'API documentations', // Description (optional)
+    },
+    basePath: '/', // Base path (optional)
+    apis: ['src/*.js']
+};
